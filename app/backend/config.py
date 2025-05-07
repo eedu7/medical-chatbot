10eea405da17f43d1c440e135cf9bceccf6d472b
eedu7 <mueedahmad067@gmail.com>
2025-05-07 07:03:45 +0500

    Addeed the database connectino and session and transaction from write to read dataabase

---
+++ app/backend/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+class BaseConfig(BaseSettings):
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        
+        
+class Config(BaseConfig):
+    POSTGRES_URL: str
+    TEST_POSTGRES_URL: str
+    
+config: Config = Config()
+
+
+print(config.POSTGRES_URL)
+print(config.TEST_POSTGRES_URL)
