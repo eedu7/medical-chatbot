8ca8b605b99ef2f42922f620fbf9e30095a2b6da
eedu7 <mueedahmad067@gmail.com>
2025-05-07 07:11:53 +0500

    Added middleware to the fastapi server

--- app/backend/server.py
+++ app/backend/server.py
@@ -1,18 +1,28 @@
+from typing import List
+
 from fastapi import FastAPI
+from fastapi.middleware import Middleware
 
 from api import router
+from middleware import SQLAlchemyMiddleware
 
 
 def init_routers(app_: FastAPI) -> None:
     app_.include_router(router)
 
 
+def make_middleware() -> List[Middleware]:
+    return [Middleware(SQLAlchemyMiddleware)]
+
+
 def create_app() -> FastAPI:
     app_ = FastAPI(
         title="AI-Powered Multi-Cancer Detection and Medical Chatbot",
         version="1.0.0",
+        middleware=make_middleware(),
     )
     init_routers(app_=app_)
+
     return app_
 
 
