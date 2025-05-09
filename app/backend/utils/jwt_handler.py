72feec77c755950c1f0a07291b594b44380d1b19
eedu7 <mueedahmad067@gmail.com>
2025-05-09 08:29:18 +0500

    Added and created the JWTHandler for creatng and decoding jwt token

---
+++ app/backend/utils/jwt_handler.py
@@ -0,0 +1,83 @@
+from uuid import uuid4
+from typing import Any, Dict
+from datetime import timedelta
+from enum import StrEnum
+
+from fastapi import HTTPException, status
+from jose import ExpiredSignatureError, JWTError, jwt
+
+from config import config
+from .current_datetime import get_current_datetime
+
+
+
+
+class TokenType(StrEnum):
+    ACCESS_TOKEN = "access_token"
+    REFRESH_TOKEN = "refresh_token"
+
+
+class JWTHandler:
+    secret_key: str = config.JWT_SECRET_KEY
+    algorithm: str = config.JWT_ALGORITHM
+    expire_minutes: int = config.JWT_EXPIRE_MINUTES
+
+    @classmethod
+    def encode(
+        cls,
+        payload: Dict[str, Any],
+        token_type: TokenType = TokenType.ACCESS_TOKEN,
+    ) -> str:
+        time_of_encoding = get_current_datetime()
+
+        expire = time_of_encoding + timedelta(minutes=cls.expire_minutes)
+
+        if token_type == TokenType.REFRESH_TOKEN:
+            expire = time_of_encoding + timedelta(minutes=cls.expire_minutes * 7)
+
+        jti = str(uuid4())
+
+        payload.update({"exp": expire, "jti": jti, "iat": time_of_encoding})
+
+        return jwt.encode(payload, cls.secret_key, algorithm=cls.algorithm)
+
+    @classmethod
+    def decode(cls, token: str) -> Dict[str, Any]:
+        try:
+            return jwt.decode(token, cls.secret_key, algorithms=[cls.algorithm])
+        except ExpiredSignatureError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail={"message": "Expired token"},
+            )
+
+        except JWTError as e:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail={"message": "Invalid token"},
+            )
+        except Exception:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail={"message": "Invalid token",}
+            )
+
+    @classmethod
+    def decode_expired(cls, token: str) -> Dict[str, Any]:
+        try:
+            return jwt.decode(
+                token,
+                cls.secret_key,
+                algorithms=[cls.algorithm],
+                options={"verify_exp": False},
+            )
+        except JWTError as e:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail={"message": "Invalid token"},
+            )
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail={"message": "Invalid token"}
+            )
