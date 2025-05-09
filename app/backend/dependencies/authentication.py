54a4da30d0013f404be0edc5994adec4e1a8851a
eedu7 <mueedahmad067@gmail.com>
2025-05-09 08:46:16 +0500

    Implement AuthenticationRequired class to enforce token-based authentication

---
+++ app/backend/dependencies/authentication.py
@@ -0,0 +1,12 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+
+
+class AuthenticationRequired:
+    def __call__(self, token: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False))):
+        
+        if not token:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail={
+                "message": "No token provided"
+            })
